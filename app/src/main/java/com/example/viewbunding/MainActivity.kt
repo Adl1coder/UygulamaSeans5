dc96e5c49882b280bf1cec84911cbf97a3422ad4
Adl1coder <adilegunngorr@gmail.com>
2023-08-10 10:05:11 +0300

    uygulama seansı 2 değişiklikler

--- app/src/main/java/com/example/viewbunding/MainActivity.kt
+++ app/src/main/java/com/example/viewbunding/MainActivity.kt
@@ -10,25 +10,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         tasarim = ActivityMainBinding.inflate(layoutInflater)
         setContentView(tasarim.root)
         val editTextAd=tasarim.editTextText
             val editTextSoyad= tasarim.editTextText2
         val editTextTcNo=tasarim.editTextText3
-        tasarim.button.setOnClickListener{
-            if(editTextAd.text.isEmpty()||editTextSoyad.text.isEmpty()||editTextTcNo.text.isEmpty()){
-              Snackbar.make(it,"kayıt alanını doldurunuz", Snackbar.LENGTH_SHORT)
-                }
-                else{
 
-
-            }
-
-
-        }
-
-        tasarim.button.setOnClickListener {
-            Snackbar.make(it, "Merhaba Kayıt Talebiniz Alınmıştır", Snackbar.LENGTH_SHORT).show()
-        }
     }
 }
